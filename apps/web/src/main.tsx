f711d54a010ba32044833a4e3ac3b2bc2f94b3ed
Ethan Chen <e263chen@uwaterloo.ca>
2025-07-18 23:54:15 -0400

    idk

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = document.getElementById("root");
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
